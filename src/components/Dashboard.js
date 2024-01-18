aa674011ef15fc16341e916965b55d7248722420
Victoria Wong <victoria.ys.wong@gmail.com>
2024-01-18 12:41:31 -0500

    Create and implement Loading component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,20 @@
-import React, { Component } from "react";
-
-import classnames from "classnames";
+import React, { Component } from "react"
+import Loading from "./Loading"
+import classnames from "classnames"
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+	state = {
+		loading: true,
+	}
 
-    return <main className={dashboardClasses} />;
-  }
+	render() {
+		const dashboardClasses = classnames("dashboard")
+
+		if (this.state.loading) {
+			return <Loading />
+		}
+
+		return <main className={dashboardClasses} />
+	}
 }
-
-export default Dashboard;
+export default Dashboard
