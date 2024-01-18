f1fcfbcfc9ece359c611b5ed11487a7b0978f9d0
Victoria Wong <victoria.ys.wong@gmail.com>
2024-01-18 12:46:26 -0500

    create and implement Panel component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,33 @@
 import React, { Component } from "react"
 import Loading from "./Loading"
+import Panel from "./Panel"
 import classnames from "classnames"
 
-class Dashboard extends Component {
-	state = {
-		loading: true,
-	}
+const data = [
+	{
+		id: 1,
+		label: "Total Photos",
+		value: 10,
+	},
+	{
+		id: 2,
+		label: "Total Topics",
+		value: 4,
+	},
+	{
+		id: 3,
+		label: "User with the most uploads",
+		value: "Allison Saeng",
+	},
+	{
+		id: 4,
+		label: "User with the least uploads",
+		value: "Lukas Souza",
+	},
+]
 
+class Dashboard extends Component {
+	state = { loading: false }
 	render() {
 		const dashboardClasses = classnames("dashboard")
 
@@ -14,7 +35,17 @@ class Dashboard extends Component {
 			return <Loading />
 		}
 
-		return <main className={dashboardClasses} />
+		const panels = data.map((panel) => (
+			<Panel
+				key={panel.id}
+				id={panel.id}
+				label={panel.label}
+				value={panel.value}
+			/>
+		))
+
+		return <main className={dashboardClasses}>{panels}</main>
 	}
 }
+
 export default Dashboard
